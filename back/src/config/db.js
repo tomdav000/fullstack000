5c61b7ef80285959ce27b182e13db66771027e70
Thomas J. Davis <thomas.junior.davis@gmail.com>
2023-12-07 21:13:26 -0600

    update db config

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB,process.env.ROOT,process.env.PW,{
+const sequelize = new Sequelize(process.env.DB,process.env.DB_USER,process.env.PW,{
 	host:process.env.HOST,
 	dialect:'mysql'
 })
